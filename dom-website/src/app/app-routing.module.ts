c7253d45db9c85c4c71ddbae78d74f4990fe8133
xDomanik <johnsonldomanik@gmail.com>
2021-04-03 17:05:08 -0400

    Finished project

--- dom-website/src/app/app-routing.module.ts
+++ dom-website/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent},
+  
 ]
 
 @NgModule({
